4a6c31777c584c48b8ed7d3d6c8c26a2d76bb1ce
Jean-Marc ANDRE <jeanmarc.jim.andre@gmail.com>
2014-12-02 14:30:11 +0100

    Improved error handling and logging

--- cmssysadmin/foreman/foremanrestapi.py
+++ cmssysadmin/foreman/foremanrestapi.py
@@ -1,30 +1,49 @@
 import requests
 import json
 import urllib
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+def formatUrl(host, args):
+	url = 'https://{0}/api'.format(host)
+	for i in args:
+		url = "{0}/{1}".format(url, i)
+	return url
+
+class NotFound(Exception):
+	def __init__(self, msg):
+		Exception.__init__(self, msg)
+
+def checkResponse(response):
+	if response.status_code == 200:
+		pass
+	elif response.status_code == 404:
+		raise NotFound("Object not found: {0} {1}".format(response.status_code, response.reason))
+	else:
+		raise Exception("Something went wrong with Foreman REST API: {0} {1}".format(response.status_code, response.reason))
 
 class ForemanRestAPI(object):
 	headers = { 'Content-Type': 'application/json', 'Accept': 'application/json;version=2' }
 
 	def get(self, *args):
-		url = 'https://{0}/api'.format(self.host)
-		for i in args:
-			url = "{0}/{1}".format(url, i)
+		url = formatUrl(self.host, args)
+		logger.debug("Sending GET to %s", url)
 		response = requests.get(url, verify=False,
 				headers=self.headers,
 				auth=(self.user, self.passwd))
-		if response.status_code != 200:
-			raise Exception("Something went wrong with the REST API request: {0} {1}".format(response.status_code, response.reason))
+		checkResponse(response)
 		return response
 
 	def post(self, payload = {}, *args):
-		url = 'https://{0}/api'.format(self.host)
-		for i in args:
-			url = "{0}/{1}".format(url, i)
+		url = formatUrl(self.host, args)
+		logger.debug("Sending POST to %s", url)
 		response = requests.post(url, data=json.dumps(payload), verify=False,
 				headers=self.headers,
 				auth=(self.user, self.passwd))
-		if response.status_code != 200:
-			raise Exception("Something went wrong with the REST API request: {0} {1}".format(response.status_code, response.reason))
+		checkResponse(response)
+		return response
 
 	def getHostGroupId(self, hostgroupName):
 		response = self.get('hostgroups', hostgroupName)
@@ -39,7 +58,7 @@ class ForemanRestAPI(object):
 				'build': build
 			}
 		}
-		self.post(payload, 'hosts')
+		return self.post(payload, 'hosts').json()
 
 	def getHost(self, hostname):
 		response = self.get('hosts', hostname)
