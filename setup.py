fb1e184bbf1ae490332191f8b314a5ed840bebd3
Jean-Marc ANDRE <jeanmarc.jim.andre@gmail.com>
2015-04-13 17:12:34 +0200

    Improved help message

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ from setuptools import setup,find_packages
 setup(
         name='python-cmssysadmin',
         data_files=[('/etc', ['cmssysadmin/cmssysadmin.conf'])],
-        scripts=['scripts/autoregister', 'scripts/get_token'],
-        version='0.3',
+        scripts=['scripts/autoregister', 'scripts/get_token', 'scripts/checks'],
+        version='0.4',
         description='CMS Sysadmin python tools',
         author='Jean-Marc ANDRE',
         author_email='jean-marc.andre@cern.ch',
-        packages=['cmssysadmin', 'cmssysadmin.foreman', 'cmssysadmin.landb'],
+        packages=['cmssysadmin', 'cmssysadmin.foreman', 'cmssysadmin.landb', 'cmssysadmin.checks'],
         url='https://github.com/desmo999r/cmssysadmin',
 )
 # vim: set ts=8 sw=8 tw=0 et :
